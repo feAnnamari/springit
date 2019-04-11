6667ac3196246c8d4d936704972ffd35629c6a94
feAnnamari <fe.annamari@gmail.com>
2019-04-11 15:20:21 +0200

    Adding mappings from link->comment

--- src/main/java/domain/Link.java
+++ src/main/java/domain/Link.java
@@ -3,7 +3,10 @@ package domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import java.util.ArrayList;
+import java.util.List;
 import io.micrometer.core.lang.NonNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,7 +24,8 @@ public class Link {
 	@NonNull
 	private String url;
 	
-	//comments
+	@OneToMany(mappedBy = "link")
+	private List<Comment> comments = new ArrayList<>();
 	
 	
 }
